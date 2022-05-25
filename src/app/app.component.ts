10588ae6b6ed7803e8b11052f4467a62bb7735e7
Egemen Ozmen <72514229+neronege@users.noreply.github.com>
2022-05-26 00:15:00 +0300

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
  
 import { BackendService } from './backend.service';
-import { category } from './models/category';
+import  category from './models/category';
 
  
 @Component({
@@ -16,32 +16,64 @@ export class AppComponent implements OnInit {
   }
   
   category : category = {
+    entities: [
+      {
+        id: 1,
+        title: "",
+        description: "",
+        password: "",
+        repassword: ""
+      },
+      {
+        id: 2,
+        title: "",
+        description: "",
+        password: "",
+        repassword: ""
+      },
+      {
+        id: 5,
+        title: "",
+        description: "",
+        password: "",
+        repassword: ""
+      },
+      {
+        id: 6,
+        title: "",
+        description: "",
+        password: "",
+        repassword: ""
+      },
+      {
+        id: 7,
+        title: "",
+        description: "",
+        password: "",
+        repassword: ""
+      },
+      {
+        id: 8,
+        title: "",
+        description: "",
+        password: "",
+        repassword: ""
+      },
+    ]
     
-    entities:[{
-        id:null,
-        title: '',
-        description: '',
-        password: '',
-        repassword: ''}]
-   
   };
-
-
-  
-  
     
   constructor(private BackendService:BackendService){
 
   }
   
-
   ngOnInit(): void {
    
    this.BackendService.getRepos().subscribe(
      (response)=> {
       
         this.category = response;
-        console.log(this.category.entities[0])
+        console.log(this.category.entities[1])
          
       },
      (err)=> {
@@ -49,17 +81,19 @@ export class AppComponent implements OnInit {
      }
    )
   }
+
  
   
   
   
    getClick(){
-     
-    return this.category.entities[0].title
+   
+    console.log(this.category.entities[4])
     
      }
      putClick(){
-   
+      debugger;
+      var note = this.category
     }
     postClick(){
      
--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
  
 import { HttpClient} from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { category } from './models/category';
+import category from './models/category';
+import {updateCategoryRequest} from './models/updateCategoryRequest';
 
  
 
@@ -18,5 +19,16 @@ export class BackendService {
   getRepos(): Observable<category> {
     return this.HttpClient.get<category>(this.baseURL+"/api"+"/Values")
   }
- 
-}
+  // putRepos(id : number, categoryRequest:category): Observable<category> {
+  //   const updateCategoryRequest : updateCategoryRequest = {
+  //     id : categoryRequest.id,
+  //     title: categoryRequest.title,
+  //     description: categoryRequest.description,
+  //     password : categoryRequest.password,
+  //     repassword : categoryRequest.repassword
+  //     }
+  //   }
+  //   return this.HttpClient.put<category>(this.baseURL+"/api"+"/Values"+{id},updateCategoryRequest)
+  }
+
+
